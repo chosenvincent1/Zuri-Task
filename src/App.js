97860b091a92c5246830928a745b9c4cecdf61d7
mrchosen <chosenvincent@gmail.com>
2022-10-29 17:02:49 -0700

    Linked buttons to respective url

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import { FaShare } from "react-icons/fa";
+import { FaShare, FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className='Header'>
-        <FaShare className='share-icon' />
+        <img src='./images/share.png' className='share-icon' />
+        <img src='./images/dots.png' className='dot-icon' />
         <div className='profile-container'>
           <Avatar 
             alt="Chosen Vincent" 
             src="./images/chosen.jpg" className='avatar'
-            sx={{width: 80, height: 80}}
+            sx={{width: 70, height: 70}}
           />
           <h3 className='username'>Chosen Vincent</h3>
         </div>
@@ -20,26 +22,28 @@ function App() {
 
       <main className='Main'>
         <div className='link-container'>
-          <button>Twitter Link</button>
-          <button>Zuri Team</button>
-          <button>Zuri Book</button>
-          <button>Python Book</button>
-          <button>Background Check for Coder</button>
-          <button>Design Book</button>
+          <button><a href='https://www.twitter.com/chosenvincent1'>Twitter Link</a></button>
+          <button><a href='https://training.zuri.team'>Zuri Team</a></button>
+          <button><a href='http://books.zuri.team'>Zuri Book</a></button>
+          <button><a href='https://books.zuri.team/python-for-beginners?ref_id=Chosen'>Python Book</a></button>
+          <button><a href='http://background.zuri.team'>Background Check for Coder</a></button>
+          <button><a href='https://books.zuri.team/design-rules'>Design Book</a></button>
         </div>
       </main>
 
       <section className='media-container'>
-        <img 
-          src='logo512.png' 
-          alt='slack' 
-          className='slack' 
-        />
-        <img 
-          src='logo512.png' 
-          alt='github' 
-          className='github' 
-        />
+        <a href='https://app.slack.com/client/T042F7V19Q8/D04882F71F0?cdn_fallback=1'>
+          <img 
+            src='./images/slack.png'
+            alt='slack' 
+            className='slack' 
+          />
+        </a>
+        <a href='https://github.com/chosenvincent1'>
+          <FaGithub 
+            className='github'
+          />
+        </a>
       </section>
 
       <footer className="Footer">
